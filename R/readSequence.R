f5637f9e702c64628a05765d9025008c36b3c2b8
Likun Wang <wanglikun@tsinghua.edu.cn>
2011-11-20 07:05:46 +0000

    Do not use readFASTA from Biostring anymore.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ASEB@60542 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/readSequence.R
+++ R/readSequence.R
@@ -1,6 +1,39 @@
 readSequence <- function(file){
-   ff <- Biostrings::readFASTA(file,strip.descs=TRUE, checkComments=TRUE)
-   sequences <- sapply(ff, function(x) x$seq)
-   ids <- sapply(ff, function(x) x$desc)
+   rd <- read.table(file, fill=TRUE,sep="")
+   rd <- as.matrix(rd)
+   sequences <- ""
+   ids <- ""
+   id <- ""
+   sequence <- ""
+   count <- 0
+   for(i in (1:nrow(rd))){
+       line <- as.vector(rd[i,])
+       if(length(grep(">", line[1])) > 0){
+          if(id != ""){
+             count <- count+1
+             ids[count] <- id
+             sequences[count] <- sequence
+          }
+          if(nchar(line[1]) > 1){
+             id <- substring(line[1],2) 
+          }else{
+             id <- line[2]
+          }
+          sequence <- ""
+       }else{
+          for(j in (1:length(line))){
+              if(is.na(line[j])){
+                 break
+              }
+              sequence <- paste(sequence, line[j], sep="")
+              sequence <- gsub("\\s+","",sequence)
+          }
+       }
+   }
+   if(id != ""){
+      count <- count+1
+      ids[count] <- id
+      sequences[count] <- sequence
+   }
    SequenceInfo(sequences, ids)
 }
