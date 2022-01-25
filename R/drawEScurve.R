a04a8c3f80e597d07af1acd90af2e735c87a433c
Likun Wang <wanglikun@tsinghua.edu.cn>
2022-01-25 13:18:55 +0800

    Fix a bug (-> as.numeric(indexes[max_y_index+1]))

--- R/drawEScurve.R
+++ R/drawEScurve.R
@@ -54,8 +54,8 @@ function(curveInfoDataFrame, sites=NULL, max_p_value=0.1, min_es=0.2, outputDir=
       plot(as.numeric(indexes[2:potins_num]),y=as.numeric(values[2:potins_num]),type="l",ylim=c(-1,1),xlab=paste("site: ", indexes[1],sep=""),ylab="ES Score",col=2,lwd=3)
       #lines(c(0,max_x),c(0.9,0.9),lwd=3)
       lines(c(0,max_x),c(0,0),lty=2,col=8)
-      lines(c(-1000,indexes[max_y_index+1]),c(max_y,max_y),lwd=1,lty=2)
-      lines(c(indexes[max_y_index+1],indexes[max_y_index+1]),c(max(-0.7,max_y-0.7),min(max_y+0.7,0.7)),lwd=1,lty=2)
+      lines(c(-1000,as.numeric(indexes[max_y_index+1])),c(max_y,max_y),lwd=1,lty=2)
+      lines(c(as.numeric(indexes[max_y_index+1]),as.numeric(indexes[max_y_index+1])),c(max(-0.7,max_y-0.7),min(max_y+0.7,0.7)),lwd=1,lty=2)
       text(as.numeric(indexes[max_y_index+1])+0.05, max_y, max_y,pos=4)
       
       for(j in (2:potins_num)){
